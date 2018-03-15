a105618f75e40e5334b6d61b5d7df206dcfa1cfd
lallhar <37359355+lallhar@users.noreply.github.com>
2018-03-15 10:38:20 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby</p>
     <p>Now go build something great.</p>
+  <p>Paragraph changing test hello world!! </p>
     <p>Netlify practise session 1</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
